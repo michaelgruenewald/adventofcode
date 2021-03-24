f3ea1701c3bb4997dbcdd06b820ad74552f946de
Michael Gruenewald <mail@michaelgruenewald.eu>
2021-03-24 20:49:53 +0100

    Convert day1 to be unittestable
    This executes my personal puzzle with
    
        python day1.py
    
    and the example with
    
        python -m unittest day1.py
    
    now.

--- 2020/day1.py
+++ 2020/day1.py
@@ -1,6 +1,28 @@
 #!/usr/bin/env python
+import unittest
 
-data = [int(x) for x in open("input1.txt").read().splitlines()]
 
-print(next(x * y for x in data for y in data if x + y == 2020))
-print(next(x * y * z for x in data for y in data for z in data if x + y + z == 2020))
+def part1(data):
+    return next(x * y for x in data for y in data if x + y == 2020)
+
+
+def part2(data):
+    return next(
+        x * y * z for x in data for y in data for z in data if x + y + z == 2020
+    )
+
+
+class Tests(unittest.TestCase):
+    EXAMPLE = [1721, 979, 366, 299, 675, 1456]
+
+    def test_part1(self):
+        self.assertEqual(514579, part1(self.EXAMPLE))
+
+    def test_part2(self):
+        self.assertEqual(241861950, part2(self.EXAMPLE))
+
+
+if __name__ == "__main__":
+    input1 = [int(x) for x in open("input1.txt").read().splitlines()]
+    print(part1(input1))
+    print(part2(input1))
