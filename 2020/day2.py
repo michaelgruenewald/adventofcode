1e8cbc6f567438754959ed35eeb1cfe81d0347e9
Michael Gruenewald <mail@michaelgruenewald.eu>
2021-03-24 20:22:36 +0100

    Reformat and refactor day2

--- 2020/day2.py
+++ 2020/day2.py
@@ -3,9 +3,11 @@ import re
 
 lines = open("input2.txt").read().splitlines()
 
+INPUT_PATTERN = re.compile(r"^(\d+)-(\d+) ([a-z]): ([a-z]+)$")
+
 valid, invalid = 0, 0
 for line in lines:
-    lower, upper, letter, password = re.match(r'^(\d+)-(\d+) ([a-z]): ([a-z]+)$', line).groups()
+    lower, upper, letter, password = INPUT_PATTERN.match(line).groups()
 
     if int(lower) <= password.count(letter) <= int(upper):
         valid += 1
@@ -17,9 +19,9 @@ print(valid, invalid)
 
 valid, invalid = 0, 0
 for line in lines:
-    first, second, letter, password = re.match(r'^(\d+)-(\d+) ([a-z]): ([a-z]+)$', line).groups()
+    first, second, letter, password = INPUT_PATTERN.match(line).groups()
 
-    if (password[int(first)-1] == letter) != (password[int(second)-1] == letter):
+    if (password[int(first) - 1] == letter) != (password[int(second) - 1] == letter):
         valid += 1
     else:
         invalid += 1
