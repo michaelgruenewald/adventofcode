e51680a9e2eaab37284a164f8f43112c1b4f78b2
Michael Gruenewald <mail@michaelgruenewald.eu>
2021-03-24 20:27:08 +0100

    Refactor day25

--- 2020/day25.py
+++ 2020/day25.py
@@ -3,6 +3,7 @@
 card_pk, door_pk = 5764801, 17807724
 card_pk, door_pk = 8335663, 8614349
 
+
 def transform(subject, loopsize):
     v = 1
     for i in range(loopsize):
@@ -17,9 +18,13 @@ def all_transforms(subject):
         v = (v * subject) % 20201227
 
 
+loops = (i for i, v in enumerate(all_transforms(7)) if v in (card_pk, door_pk))
+first_loop, second_loop = next(loops), next(loops)
 
-card_loop = next(i for i, v in enumerate(all_transforms(7)) if v == card_pk)
-door_loop = next(i for i, v in enumerate(all_transforms(7)) if v == door_pk)
+if transform(7, first_loop) == card_pk:
+    card_loop, door_loop = first_loop, second_loop
+else:
+    door_loop, card_loop = first_loop, second_loop
 
 card_key = transform(door_pk, card_loop)
 door_key = transform(card_pk, door_loop)
