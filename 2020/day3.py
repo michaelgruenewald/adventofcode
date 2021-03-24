81d6d4fd1f45ebab0a3391a5bba4913cdef3e07d
Michael Gruenewald <mail@michaelgruenewald.eu>
2021-03-24 20:22:57 +0100

    Reformat day3

--- 2020/day3.py
+++ 2020/day3.py
@@ -17,10 +17,14 @@ lines = """\
 lines = open("input3.txt").read().splitlines()
 
 # part 1
-print(sum(line[(i*3)%len(line)] == "#" for i, line in enumerate(lines)))
+print(sum(line[(i * 3) % len(line)] == "#" for i, line in enumerate(lines)))
 
 # part 2
 results = []
 for right, down in ((1, 1), (3, 1), (5, 1), (7, 1), (1, 2)):
-    results.append(sum(line[(i*right)%len(line)] == "#" for i, line in enumerate(lines[::down])))
+    results.append(
+        sum(
+            line[(i * right) % len(line)] == "#" for i, line in enumerate(lines[::down])
+        )
+    )
 print(math.prod(results), results)
