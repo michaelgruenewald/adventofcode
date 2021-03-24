f62a30633f5216a662cea3428a26983580831b0c
Michael Gruenewald <mail@michaelgruenewald.eu>
2021-03-24 20:22:24 +0100

    Reformat day1

--- 2020/day1.py
+++ 2020/day1.py
@@ -2,5 +2,5 @@
 
 data = [int(x) for x in open("input1.txt").read().splitlines()]
 
-print(next(x*y for x in data for y in data if x+y==2020))
-print(next(x*y*z for x in data for y in data for z in data if x+y+z==2020))
+print(next(x * y for x in data for y in data if x + y == 2020))
+print(next(x * y * z for x in data for y in data for z in data if x + y + z == 2020))
