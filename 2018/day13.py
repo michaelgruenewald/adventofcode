00b2fc660a0a9b8cf1ec956966b48e42779a491f
Michael Gruenewald <mail@michaelgruenewald.eu>
2021-04-06 12:01:22 +0200

    Finish day13

--- 2018/day13.py
+++ 2018/day13.py
@@ -1,19 +1,91 @@
 #!/usr/bin/env pytohn
+import itertools
 
 data = r"""/->-\
 |   |  /----\
 | /-+--+-\  |
 | | |  | v  |
 \-+-/  \-+--/
-  \------/   
+  \------/
 """
 
+data = r"""/>-<\
+|   |  
+| /<+-\
+| | | v
+\>+</ |
+  |   ^
+  \<->/"""
+
+data = open("input13.txt").read()
+
+UP = (-1, 0)
+DOWN = (1, 0)
+LEFT = (0, -1)
+RIGHT = (0, 1)
+
 CART_SYMBOLS = {
-    ">": ((0, 1), "-"),
-    "<": ((0, -1), "-"),
-    "^": ((-1, 0), "|"),
-    "v": ((1, 0), "|"),
+    ">": (RIGHT, "-"),
+    "<": (LEFT, "-"),
+    "^": (UP, "|"),
+    "v": (DOWN, "|"),
 }
+
+
+class Cart:
+    def __init__(self, row, col, symbol):
+        self.row = row
+        self.col = col
+        self.direction = CART_SYMBOLS[symbol][0]
+        self.turn_state = 0
+
+    def tick(self):
+        self.row, self.col = self.row + self.direction[0], self.col + self.direction[1]
+        track = tracks[self.row, self.col]
+        if self.direction[0] == 0 and track == "-":
+            pass
+        elif self.direction[1] == 0 and track == "|":
+            pass
+        elif track == "+":
+            if self.turn_state == 0:
+                self.direction = -self.direction[1], self.direction[0]  # left
+            elif self.turn_state == 2:
+                self.direction = self.direction[1], -self.direction[0]  # right
+            self.turn_state = (self.turn_state + 1) % 3
+        elif (
+            self.direction == RIGHT
+            and track == "\\"
+            or self.direction == LEFT
+            and track == "/"
+        ):
+            self.direction = DOWN
+        elif (
+            self.direction == RIGHT
+            and track == "/"
+            or self.direction == LEFT
+            and track == "\\"
+        ):
+            self.direction = UP
+        elif (
+            self.direction == DOWN
+            and track == "\\"
+            or self.direction == UP
+            and track == "/"
+        ):
+            self.direction = RIGHT
+        elif (
+            self.direction == DOWN
+            and track == "/"
+            or self.direction == UP
+            and track == "\\"
+        ):
+            self.direction = LEFT
+        else:
+            assert False
+
+    def __repr__(self):
+        return f"<Cart {self.col},{self.row} d={self.direction}>"
+
 
 tracks = {}
 carts = []
@@ -23,27 +95,27 @@ for c in data:
     if c == "\n":
         row, col = row + 1, 0
         continue
-    else:
-        col += 1
 
     if c in CART_SYMBOLS:
-        cart, c = CART_SYMBOLS[c]
-        carts.append((row, col, cart[0], cart[1]))
+        carts.append(Cart(row, col, c))
+        c = CART_SYMBOLS[c][1]
 
     if c in r"\/-|+":
-        tracks[(row, col)] = c
+        tracks[row, col] = c
     elif c == " ":
         pass
     else:
         assert False
 
-while True:
-    carts.sort()
-    for i in range(len(carts)):
-        row, col, drow, dcol = carts.pop(0)
-        row += drow
-        col += dcol
+    col += 1
 
-        carts.append(cart)
-    print(carts)
-    break
+while len(carts) > 1:
+    for cart in sorted(carts, key=lambda cart: (cart.row, cart.col)):
+        cart.tick()
+        crashing = [c for c in carts if c is not cart and c.row == cart.row and c.col == cart.col]
+        if crashing:
+            for c in crashing:
+                carts.remove(c)
+            carts.remove(cart)
+
+print(carts)
