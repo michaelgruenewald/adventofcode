b9feed219ed36cbfb83c00fbde07fac271561dc0
Michael Gruenewald <mail@michaelgruenewald.eu>
2021-12-04 11:22:25 +0100

    Day 3

---
+++ 2021/src/day3.rs
@@ -0,0 +1,78 @@
+pub fn part1(input: &str) -> usize {
+    let numbers = input.split_terminator("\n").collect::<Vec<_>>();
+    let length = numbers.len();
+    let width = numbers[0].len();
+
+    let mut sums = vec![0usize; width];
+    for n in numbers {
+        for (i, c) in n.chars().enumerate() {
+            sums[i] += c.to_digit(2).unwrap() as usize;
+        }
+    }
+
+    let gamma = sums
+        .iter()
+        .fold(0, |a, sum| a * 2 + (if *sum > length / 2 { 1 } else { 0 }));
+    let epsilon = gamma ^ ((2 << width - 1) - 1);
+
+    gamma * epsilon
+}
+
+pub fn part2(input: &str) -> usize {
+    let mut numbers = input.split_terminator("\n").collect::<Vec<_>>();
+    numbers.sort();
+
+    let mut s = &numbers[..];
+    for pos in 0.. {
+        let split = s.partition_point(|&x| x.chars().nth(pos).unwrap() == '0');
+        s = if split > s.len() / 2 {
+            &s[..split]
+        } else {
+            &s[split..]
+        };
+        if s.len() == 1 {
+            break;
+        }
+    }
+    let oxygen = usize::from_str_radix(s[0], 2).unwrap();
+
+    let mut s = &numbers[..];
+    for pos in 0.. {
+        let split = s.partition_point(|&x| x.chars().nth(pos).unwrap() == '0');
+        s = if split > s.len() / 2 {
+            &s[split..]
+        } else {
+            &s[..split]
+        };
+        if s.len() == 1 {
+            break;
+        }
+    }
+    let co2 = usize::from_str_radix(s[0], 2).unwrap();
+
+    oxygen * co2
+}
+
+#[cfg(test)]
+const EXAMPLE: &str = "00100
+11110
+10110
+10111
+10101
+01111
+00111
+11100
+10000
+11001
+00010
+01010";
+
+#[test]
+fn test_part1() {
+    assert_eq!(198, part1(EXAMPLE));
+}
+
+#[test]
+fn test_part2() {
+    assert_eq!(230, part2(EXAMPLE));
+}
--- 2021/src/main.rs
+++ 2021/src/main.rs
@@ -1,9 +1,12 @@
 mod day1;
 mod day2;
+mod day3;
 
 fn main() {
     let input1 = include_str!("input1.txt");
     println!("{} {}", day1::part1(input1), day1::part2(input1));
     let input2 = include_str!("input2.txt");
     println!("{} {}", day2::part1(input2), day2::part2(input2));
+    let input3 = include_str!("input3.txt");
+    println!("{} {}", day3::part1(input3), day3::part2(input3));
 }
