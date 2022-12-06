07b8df7de4cd1bfb0669b05ccf4619f9f7b544ce
Michael Gruenewald <mail@michaelgruenewald.eu>
2022-12-06 18:22:07 +0100

    Refactoring

--- 2022/src/Day4.hs
+++ 2022/src/Day4.hs
@@ -1,14 +1,13 @@
 module Day4 (main) where
 
+import Data.Bifunctor (Bifunctor (bimap))
 import Data.List (intersect, isSubsequenceOf)
 
 parseRange :: [Char] -> [Int]
-parseRange t = enumFromTo (read l) (read (tail r)) where (l, r) = break (== '-') t
+parseRange = uncurry enumFromTo . bimap read (read . tail) . break (=='-')
 
 parse :: String -> [([Int], [Int])]
-parse =
-  let parseline line = (parseRange l, parseRange (tail r)) where (l, r) = break (== ',') line
-   in map parseline . lines
+parse = map (bimap parseRange (parseRange . tail) . break (== ',')) . lines
 
 part1 :: String -> Int
 part1 = length . filter (\(a, b) -> isSubsequenceOf a b || isSubsequenceOf b a) . parse
