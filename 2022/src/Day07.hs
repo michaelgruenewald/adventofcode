d9061964a8c5abe34919d929128823a8c4bb2df9
Michael Gruenewald <mail@michaelgruenewald.eu>
2022-12-10 22:29:38 +0100

    Use lenses for day 7

--- 2022/src/Day07.hs
+++ 2022/src/Day07.hs
@@ -1,35 +1,43 @@
+{-# LANGUAGE TemplateHaskell #-}
+
 module Day07 (main) where
 
+import Control.Lens
 import Data.Char (isDigit)
 import Data.List (foldl', isPrefixOf)
-import qualified Data.Map as M
+import Data.Map (Map, adjust, alter, empty, filterWithKey, keys)
 import Data.Maybe (fromMaybe)
 
-data ParseState = ParseState {cwd :: [String], dirSizes :: M.Map [String] Int}
+data ParseState = ParseState {_cwd :: [String], _dirSizes :: Map [String] Int}
 
-parse :: String -> M.Map [String] Int
-parse = dirSizes . foldl' step initial . lines
+makeLenses ''ParseState
+
+parse :: String -> Map [String] Int
+parse = (^. dirSizes) . foldl' step initial . lines
   where
-    initial = ParseState [] M.empty
-    step state line = case words line of
-      ["$", "cd", ".."] -> state {cwd = init (cwd state)}
-      ["$", "cd", "/"] -> ensureDir $ state {cwd = []}
-      ["$", "cd", name] -> ensureDir $ state {cwd = cwd state ++ [name]}
-      ["$", "ls"] -> state
-      "$" : _ -> error ("Unknown command: " ++ line)
-      ["dir", name] -> state
-      [size, name] | all isDigit size -> state {dirSizes = M.adjust (+ read size) (cwd state) (dirSizes state)}
-      _ -> error ("Unknown line: " ++ line)
-    ensureDir state = state {dirSizes = M.alter (Just . fromMaybe 0) (cwd state) (dirSizes state)}
+    initial = ParseState [] empty
+    step state line =
+      ensureDir
+        $ case words line of
+          ["$", "cd", "/"] -> set cwd []
+          ["$", "cd", ".."] -> over cwd init
+          ["$", "cd", name] -> over cwd (++ [name])
+          ["$", "ls"] -> id
+          "$" : _ -> error ("Unknown command: " ++ line)
+          ["dir", name] -> id
+          [size, name] | all isDigit size -> over dirSizes (adjust (+ read size) (state ^. cwd))
+          _ -> error ("Unknown line: " ++ line)
+        $ state
+    ensureDir state = over dirSizes (alter (Just . fromMaybe 0) (state ^. cwd)) state
 
-treeSize :: M.Map [String] Int -> [String] -> Int
-treeSize sizes root = sum $ M.filterWithKey (\dir _ -> root `isPrefixOf` dir) sizes
+treeSize :: Map [String] Int -> [String] -> Int
+treeSize sizes root = sum $ filterWithKey (\dir _ -> root `isPrefixOf` dir) sizes
 
 part1 :: String -> Int
-part1 s = sum . filter (<= 100000) $ map (treeSize dirs) (M.keys dirs) where dirs = parse s
+part1 s = sum . filter (<= 100000) $ map (treeSize dirs) (keys dirs) where dirs = parse s
 
 part2 :: String -> Int
-part2 s = minimum . filter (>= treeSize dirs [] + 30000000 - 70000000) $ map (treeSize dirs) (M.keys dirs) where dirs = parse s
+part2 s = minimum . filter (>= treeSize dirs [] + 30000000 - 70000000) $ map (treeSize dirs) (keys dirs) where dirs = parse s
 
 main :: IO ()
 main = do
