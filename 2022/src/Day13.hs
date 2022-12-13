7bac496766cba7aa0ada226632de1d929ee172f1
Michael Gruenewald <mail@michaelgruenewald.eu>
2022-12-13 18:39:09 +0100

    Day 13

---
+++ 2022/src/Day13.hs
@@ -0,0 +1,51 @@
+module Day13 (main) where
+
+import Data.Functor ((<&>))
+import qualified Data.Functor.Identity
+import Data.List (elemIndices, findIndices, sort)
+import qualified Text.Parsec as P
+
+data Packet = N Int | L [Packet] deriving (Eq, Show)
+
+instance Ord Packet where
+  compare (N left) (N right) = compare left right
+  compare (N left) right = compare (L [N left]) right
+  compare left (N right) = compare left (L [N right])
+  compare (L []) (L []) = EQ
+  compare (L []) (L (_ : _)) = LT
+  compare (L (_ : _)) (L []) = GT
+  compare (L (left : ls)) (L (right : rs))
+    | left == right = compare (L ls) (L rs)
+    | otherwise = compare left right
+
+packetParser :: P.ParsecT String u Data.Functor.Identity.Identity Packet
+packetParser =
+  P.choice
+    [ P.many1 P.digit <&> (N . read),
+      P.char '[' >> P.sepBy packetParser (P.string ",") <* P.char ']' <&> L
+    ]
+
+fileParser :: P.ParsecT String u Data.Functor.Identity.Identity [(Packet, Packet)]
+fileParser =
+  P.sepBy1
+    ( do
+        p1 <- packetParser <* P.endOfLine
+        p2 <- packetParser <* P.endOfLine
+        return (p1, p2)
+    )
+    P.endOfLine
+
+parse :: String -> [(Packet, Packet)]
+parse = either (error . show) id . P.parse fileParser "input13.txt"
+
+part1 :: String -> Int
+part1 = sum . map (+ 1) . elemIndices LT . map (uncurry compare) . parse
+
+part2 :: String -> Int
+part2 = product . map (+ 1) . findIndices (`elem` dividers) . sort . (dividers ++) . concatMap (\(x, y) -> [x, y]) . parse where dividers = [L [L [N 2]], L [L [N 6]]]
+
+main :: IO ()
+main = do
+  contents <- readFile "src/input13.txt"
+  print $ part1 contents
+  print $ part2 contents
